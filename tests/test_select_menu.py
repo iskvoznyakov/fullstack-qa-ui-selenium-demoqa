16f52a7d3ce85e9d6af3b73dc3a54da564a811f7
iskvoznyakov <iliaskvozniakov@mail.ru>
2025-07-24 08:39:24 +0400

    feat(select-menu): complete page object and extend test coverage

--- tests/test_select_menu.py
+++ tests/test_select_menu.py
@@ -1,12 +1,10 @@
-import time
-
 import pytest
 from pages.select_menu_page import SelectMenuPage
 
 
 @pytest.mark.parametrize("value", [
     "A root option",
-"Group 1, option 1"
+    "Group 1, option 1"
 ])
 def test_select_value_from_select_value_dropdown(driver, value):
     select_menu_page = SelectMenuPage(driver)
@@ -26,3 +24,37 @@ def test_select_value_from_select_one_dropdown(driver, value):
     select_menu_page.select_value_from_select_one_dropdown(value)
     selected_value = select_menu_page.get_selected_single_value()
     assert selected_value == value, f"Выбран не {value}, а {selected_value}"
+
+
+@pytest.mark.parametrize("color", [
+    "Yellow",
+    "Blue"
+])
+def test_select_value_from_old_style_select_menu(driver, color):
+    select_menu_page = SelectMenuPage(driver)
+    select_menu_page.open()
+    selected = select_menu_page.select_value_from_old_style_select_menu(color)
+    assert selected == color, f"Ожидалось {color}, а получено {selected}"
+
+
+@pytest.mark.parametrize("values", [
+    ["Green", "Black", "Blue", "Red"],
+    ["Green", "Red"]
+])
+def test_select_value_from_multi_select_dropdown(driver, values):
+    select_menu_page = SelectMenuPage(driver)
+    select_menu_page.open()
+    select_menu_page.select_values_from_multi_select_dropdown(values)
+    selected_values = select_menu_page.get_selected_multi_values()
+    assert set(values) == set(selected_values), f"{values} не совпадают с {selected_values}"
+
+
+@pytest.mark.parametrize("values", [
+    ["volvo", "opel"],
+    ["saab", "audi"]
+])
+def test_select_value_from_standard_multi_select(driver, values):
+    select_menu_page = SelectMenuPage(driver)
+    select_menu_page.open()
+    selected = select_menu_page.select_values_from_standard_multi_select(values)
+    assert set([value.capitalize() for value in values]) == set(selected), f"{values} не выбран. Получено: {selected}"
