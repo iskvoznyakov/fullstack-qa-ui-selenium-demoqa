b774450488d836a0de79d9acaaa169e49ba3e64a
Ilya Skvoznyakov <104623164+iskvoznyakov@users.noreply.github.com>
2025-07-26 15:32:21 +0400

    feat(sortable): add another test (#28)

--- pages/sortable_page.py
+++ pages/sortable_page.py
@@ -7,33 +7,40 @@ from selenium.webdriver import ActionChains
 
 class SortablePage(BasePage):
     LIST_TAB = (By.ID, "demo-tab-list")
-    list_options = {"one": (By.XPATH, "//div[@id='demo-tabpane-list']//div[contains(text(), 'One')]"),
-                    "two": (By.XPATH, "//div[@id='demo-tabpane-list']//div[contains(text(), 'Two')]"),
-                    "three": (By.XPATH, "//div[@id='demo-tabpane-list']//div[contains(text(), 'Three')]"),
-                    "four": (By.XPATH, "//div[@id='demo-tabpane-list']//div[contains(text(), 'Four')]"),
-                    "five": (By.XPATH, "//div[@id='demo-tabpane-list']//div[contains(text(), 'Five')]"),
-                    "six": (By.XPATH, "//div[@id='demo-tabpane-list']//div[contains(text(), 'Six')]"), }
-    LIST_OPTIONS = (By.XPATH, "//div[@id='demo-tabpane-list']//div[contains(@class, 'list-group-item')]")
 
     GRID_TAB = (By.ID, "demo-tab-grid")
-    grid_options = {"one": (By.XPATH, "//div[@id='demo-tabpane-grid']//div[contains(text(), 'One')]"),
-                    "two": (By.XPATH, "//div[@id='demo-tabpane-grid']//div[contains(text(), 'Two')]"),
-                    "three": (By.XPATH, "//div[@id='demo-tabpane-grid']//div[contains(text(), 'Three')]"),
-                    "four": (By.XPATH, "//div[@id='demo-tabpane-grid']//div[contains(text(), 'Four')]"),
-                    "five": (By.XPATH, "//div[@id='demo-tabpane-grid']//div[contains(text(), 'Five')]"),
-                    "six": (By.XPATH, "//div[@id='demo-tabpane-grid']//div[contains(text(), 'Six')]"), }
-    GRID_OPTIONS = (By.XPATH, "//div[@id='demo-tabpane-grid']//div[contains(@class, 'list-group-item')]")
+
+    OPTIONS = {
+        "List": (By.XPATH, "//div[@id='demo-tabpane-list']//div[contains(@class, 'list-group-item')]"),
+        "Grid": (By.XPATH, "//div[@id='demo-tabpane-grid']//div[contains(@class, 'list-group-item')]")
+    }
 
     def open(self):
         super().open(BASE_URL + "/sortable")
 
     @log_action
-    def drag_and_drop(self, from_index, to_index):
+    def switch_to_tab(self, tab_name):
+        if tab_name == 'List':
+            self.click(self.LIST_TAB)
+        elif tab_name == 'Grid':
+            self.click(self.GRID_TAB)
+
+    @log_action
+    def drag_and_drop(self, tab_name, from_index, to_index):
+        if tab_name not in self.OPTIONS:
+            raise ValueError(f"Некорректное имя таба: {tab_name}")
+
+        elements = self.driver.find_elements(*self.OPTIONS[tab_name])
+
         (ActionChains(self.driver).
-         drag_and_drop(self.driver.find_elements(*self.LIST_OPTIONS)[from_index],
-                       self.driver.find_elements(*self.LIST_OPTIONS)[to_index]).
+         drag_and_drop(elements[from_index],
+                       elements[to_index]).
          perform())
 
     @log_action
-    def get_list_items(self):
-        return [elem.text for elem in self.driver.find_elements(*self.LIST_OPTIONS)]
+    def get_items(self, tab_name):
+        if tab_name not in self.OPTIONS:
+            raise ValueError(f"Некорректное имя таба: {tab_name}")
+
+        elements = self.driver.find_elements(*self.OPTIONS[tab_name])
+        return [elem.text for elem in elements]
