0c23a72ebe1af9edeec831f78d9225fdc1b0d1b6
Ilya Skvoznyakov <104623164+iskvoznyakov@users.noreply.github.com>
2025-08-02 10:07:21 +0400

    feat(dragabble): add another test (#35)

--- pages/dragabble_page.py
+++ pages/dragabble_page.py
@@ -8,9 +8,14 @@ from selenium.webdriver import ActionChains
 class DragabblePage(BasePage):
     TABS = {
         "simple_tab": (By.ID, "draggableExample-tab-simple"),
+        "axis_restricted_tab": (By.ID, "draggableExample-tab-axisRestriction")
     }
 
-    DRAG_ME_ELEMENT = (By.ID, "dragBox")
+    DRAGGABLE_ELEMENTS = {
+        "drag_me_element": (By.ID, "dragBox"),
+        "only_x_element": (By.ID, "restrictedX"),
+        "only_y_element": (By.ID, "restrictedY"),
+    }
 
     def open(self):
         super().open(BASE_URL + "/dragabble")
@@ -20,14 +25,14 @@ class DragabblePage(BasePage):
         self.click(self.TABS[tab_name])
 
     @log_action
-    def drag_simple_element_by_offset(self, x_offset, y_offset):
-        element = self.find(self.DRAG_ME_ELEMENT)
+    def drag_element_by_offset(self, element_name, x_offset, y_offset):
+        element = self.find(self.DRAGGABLE_ELEMENTS[element_name])
         (ActionChains(self.driver).
          drag_and_drop_by_offset(element, x_offset, y_offset).
          perform())
 
     @log_action
-    def get_simple_element_position(self):
-        element = self.find(self.DRAG_ME_ELEMENT)
+    def get_element_position(self, element_name):
+        element = self.find(self.DRAGGABLE_ELEMENTS[element_name])
         location = element.location
         return location['x'], location['y']
