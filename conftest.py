9c90968bae78463e2e7448724e367f2c66f7d3eb
Ilya Skvoznyakov <104623164+iskvoznyakov@users.noreply.github.com>
2025-08-07 08:02:10 +0400

    feat(book-store): add profile page and test for successful logout (#40)

--- conftest.py
+++ conftest.py
@@ -10,7 +10,7 @@ import allure
 def driver():
     options = Options()
     # options.add_argument("--headless")
-    # options.add_argument("--incognito")
+    options.add_argument("--incognito")
     options.add_argument("--window-size=1920,1080")
     options.page_load_strategy = 'eager'
 
