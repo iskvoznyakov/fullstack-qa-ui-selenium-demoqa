768c7267610fdd8be13c7acb1d76e3cba60e4968
iskvoznyakov <iliaskvozniakov@mail.ru>
2025-08-10 17:24:34 +0400

    feat(book-store): add test for book pagination

--- pages/books_page.py
+++ pages/books_page.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
 from config import BASE_URL
 from pages.base_page import BasePage
 from utils.decorators import log_action
@@ -8,6 +10,7 @@ class BooksPage(BasePage):
     SEARCH_FIELD = (By.ID, "searchBox")
     FIND_BUTTON = (By.ID, "basic-addon2")
     BOOK_TITLES = (By.CLASS_NAME, "action-buttons")
+    ROWS_DROPDOWN = (By.XPATH, "//select[@aria-label='rows per page']")
 
     def open(self):
         super().open(BASE_URL + "/books")
@@ -20,3 +23,8 @@ class BooksPage(BasePage):
     def find_result(self):
         books_found = self.driver.find_elements(*self.BOOK_TITLES)
         return [book.text for book in books_found]
+
+    def choose_books_per_page(self, number_of_books):
+        elem = self.find(self.ROWS_DROPDOWN)
+        select = Select(elem)
+        select.select_by_value(str(number_of_books))
--- tests/test_books.py
+++ tests/test_books.py
@@ -14,3 +14,15 @@ def test_find_books(driver, string_for_searching, result):
     books_page.find_book_by_name(string_for_searching)
     books_found = books_page.find_result()
     assert set(books_found) == set(result), f"Найденные книги: {books_found}, не совпадают с ОР: {result}"
+
+
+@pytest.mark.parametrize("number_of_books", [
+    5
+])
+def test_page_books(driver, number_of_books):
+    books_page = BooksPage(driver)
+    books_page.open()
+    books_page.choose_books_per_page(number_of_books)
+    books_found = books_page.find_result()
+    assert len(
+        books_found) == number_of_books, f"На странице отображается не {number_of_books} книг, а {len(books_found)}"
